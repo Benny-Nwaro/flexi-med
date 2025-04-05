26ef7f0861374e3830a2e1551e8802c831351f16
Benny-Nwaro <baroh3767@aol.com>
2025-04-05 19:47:45 +0100

    Added backend URL for oauth config

--- src/main/java/com/example/flexiMed/HomeController.java
+++ src/main/java/com/example/flexiMed/HomeController.java
@@ -1,7 +1,5 @@
 package com.example.flexiMed;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome! <a href='/oauth2/authorization/google'>Login with Google</a>";
-    }
-
-    @GetMapping("/user")
-    public OAuth2User getUser(@AuthenticationPrincipal OAuth2User user) {
-        return user;
+        return "Welcome to Flexi Medical Dispatch ! <a href='flexi-med-front-itcp.vercel.app'>click here to begin</a>";
     }
 }
 
