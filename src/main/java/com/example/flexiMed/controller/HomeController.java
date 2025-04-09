b54ea1bcc7f1d1d3dcefe9650635672d3d9a5399
Benny-Nwaro <baroh3767@aol.com>
2025-04-09 12:48:11 +0100

    made updates to home controller

--- src/main/java/com/example/flexiMed/controller/HomeController.java
+++ src/main/java/com/example/flexiMed/controller/HomeController.java
@@ -1,14 +1,16 @@
 package com.example.flexiMed.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(Model model) {
+        // Adding a dynamic message to the template
+        model.addAttribute("welcomeMessage", "Welcome to Flexi Medical Dispatch");
+        return "home"; // This will resolve to /templates/home.html
     }
 }
-
