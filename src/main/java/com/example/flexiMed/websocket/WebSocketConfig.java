a834f202fc5623678dcd39b530baaf647943a406
Benny-Nwaro <baroh3767@aol.com>
2025-04-10 20:35:45 +0100

    made updates to websockets fixing notification handling in production

--- src/main/java/com/example/flexiMed/websocket/WebSocketConfig.java
+++ src/main/java/com/example/flexiMed/websocket/WebSocketConfig.java
@@ -1,38 +1,40 @@
 package com.example.flexiMed.websocket;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+/**
+ * Configuration class for WebSocket messaging using STOMP.
+ * This class enables and configures WebSocket message broker capabilities for the application.
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * Registers STOMP endpoints for WebSocket connections.
+     *
+     * @param registry The StompEndpointRegistry used to register STOMP endpoints.
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/ambulance-updates")
-                .setHandshakeHandler(new UserIdHandshakeHandler())
+        registry.addEndpoint("/ws", "/ws/ambulance-updates")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
+    /**
+     * Configures the message broker for handling messages.
+     *
+     * @param registry The MessageBrokerRegistry used to configure the message broker.
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/queue", "/topic")
-                .setHeartbeatValue(new long[]{10000, 10000});
         registry.setUserDestinationPrefix("/user");
-    }
-
-    @Bean
-    public TaskScheduler taskScheduler() {
-        ThreadPoolTaskScheduler defaultTaskScheduler = new ThreadPoolTaskScheduler();
-        defaultTaskScheduler.setPoolSize(1); // You can adjust this
-        defaultTaskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
-        defaultTaskScheduler.initialize();
-        return defaultTaskScheduler;
     }
 }
